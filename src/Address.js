bfca4970c77192126700c77e05d22b1a137d09b5
nao281 <59191981+nao281@users.noreply.github.com>
2020-06-20 20:41:36 +0300

    Update Address.js

--- src/Address.js
+++ src/Address.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 
 
 import ItemForm from "./ItemForm";
-import StateDrop from "./StateDrop";
 import getCountries from"./GetCountries";
 import validate from "./ValidationRules"
 
